0325de876b3218a753fefb7182df734e7af5cc9b
a <a>
2015-05-01 21:44:00 +0900

    r05

--- SystemIdleMonitor/CheckProcess.cs
+++ SystemIdleMonitor/CheckProcess.cs
@@ -6,65 +6,83 @@ using System.IO;
 using System.Reflection;
 using System.Diagnostics;
 using System.Threading;
-
+using System.Text.RegularExpressions;
 
 namespace SystemIdleMonitor
 {
-	static class CheckProcess
-	{
-		static List<string> BlackList;
-		static CheckProcess()
-		{
-			//パス
-			string AppPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-			string AppDir = Path.GetDirectoryName(AppPath);
-			string AppName = Path.GetFileNameWithoutExtension(AppPath);
-			string txtpath = Path.Combine(AppDir, AppName + ".txt");
-
-			//initialize
-			BlackList = new List<string>();
-
-			//テキスト読込み
-			if (File.Exists(txtpath) == false) return;
-			var textList = File.ReadAllLines(txtpath).ToList();
+  static class CheckProcess
+  {
+    static List<string> BlackList;
+    static CheckProcess()
+    {
+      //パス
+      string AppPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+      string AppDir = Path.GetDirectoryName(AppPath);
+      string AppName = Path.GetFileNameWithoutExtension(AppPath);
+      string txtpath = Path.Combine(AppDir, AppName + ".txt");
 
 
-			//BlackList登録
-			BlackList = textList.Select(
-			 (line) =>
-			 {	//コメント削除
-				 int found = line.IndexOf("//");
-				 line = (-1 < found) ? line.Substring(0, found) : line;
-				 line = line.Trim();
+      //initialize
+      BlackList = new List<string>();
 
-				 //.exe削除
-				 bool haveExe = (Path.GetExtension(line).ToLower() == ".exe");
-				 line = (haveExe) ? Path.GetFileNameWithoutExtension(line) : line;
-				 return line;
-			 })
-				//空白行削除
-			 .Where((line) => string.IsNullOrWhiteSpace(line) == false)
-				//重複排除
-			 .Distinct()
-			 .ToList();
-
-		}
+      //テキスト読込み
+      if (File.Exists(txtpath) == false) return;
+      var textList = File.ReadAllLines(txtpath).ToList();
 
 
-		//BlackList内のプロセスが稼動していない？
-		public static bool NotExistBlack()
-		{
+      //BlackList作成、登録
+      BlackList = textList;
+      BlackList = BlackList.Select(
+       (line) =>
+       {  //コメント削除
+         int found = line.IndexOf("//");
+         line = (-1 < found) ? line.Substring(0, found) : line;
+         line = line.Trim();
 
-			foreach (var blk in BlackList)
-			{
-				var prclist = Process.GetProcessesByName(blk);
-				if (0 < prclist.Count()) return false;
-			}
+         //.exe削除
+         bool haveExe = (Path.GetExtension(line).ToLower() == ".exe");
+         line = (haveExe) ? Path.GetFileNameWithoutExtension(line) : line;
 
-			return true;
-		}
+         //ワイルドカードを正規表現に変換
+         line = Regex.Replace(line, @"\?", ".");
+         line = Regex.Replace(line, @"\*", ".*");
+
+         return line;
+       })
+       .Where((line) => string.IsNullOrWhiteSpace(line) == false)    //空白行削除
+       .Distinct()                                                   //重複排除
+       .ToList();
+
+    }
 
 
 
-	}
+    //BlackListのプロセスが稼動していない？
+    public static bool NotExistBlack()
+    {
+      var prclist = Process.GetProcesses();
+
+      foreach (var pattern in BlackList)
+        foreach (var prc in prclist)
+        {
+          //部分一致ではなく完全一致で検索
+          try
+          {
+            var hitBlack = Regex.IsMatch(prc.ProcessName, "^" + pattern + "$", RegexOptions.IgnoreCase);
+            if (hitBlack) return false;
+          }
+          catch
+          {
+            //patternが不正だと例外。notepad++など。
+            continue;
+          }
+        }
+
+      return true;
+    }
+
+
+
+
+  }
 }
--- SystemIdleMonitor/Program.cs
+++ SystemIdleMonitor/Program.cs
@@ -9,7 +9,6 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-
 #region region_title
 #endregion
 
@@ -17,271 +16,269 @@ using System.Threading.Tasks;
 namespace SystemIdleMonitor
 {
 
-	static class DefValue
-	{
-		public const float
-			CpuThd = 60f, HddThd = 30.0f, NetworkThd = -1.0f, Durarion = 20, Timeout = 30;
-		//CpuThd = 0f, HddThd = 0.0f, NetworkThd = 1.0f,	Durarion = 60 * 60, Timeout = -30;
-		//CpuThd = 0f, HddThd = 0.0f, NetworkThd = -1.0f, Durarion = 300, Timeout = 360;
-	}
+  static class DefValue
+  {
+    public const float
+      //           %               MiB/sec             Mbps            sec           sec  
+      CpuThd = 60f, HddThd = 30.0f, NetworkThd = -1.0f, Durarion = 20, Timeout = 30;
+  }
 
 
-	class Program
-	{
-		static SystemMonitor systemMonitor;
-		static readonly object sync = new object();
-		static float duration, timeout;							//timeout == -1 なら無期限待機
-		static DateTime startTime, lastResumeTime;
-		static bool HaveConsole;
-		static bool SystemIsSleep;
+  class Program
+  {
+    static SystemMonitor systemMonitor;
+    static readonly object sync = new object();
+    static float duration, timeout;                       //timeout = -1 なら無期限待機
+    static DateTime startTime, lastResumeTime;
+    static bool HaveConsole;
+    static bool SystemIsSleep;
 
 
-		static void Main(string[] args)
-		{
-			//テスト引数
-			//var testArgs = new List<string>();
-			//testArgs.AddRange(new string[] { "-cputhd", "40" });
-			//testArgs.AddRange(new string[] { "-hddthd", "6" });
-			//testArgs.AddRange(new string[] { "-netthd", "10" });
-			//testArgs.AddRange(new string[] { "-duration", "20" });
-			//testArgs.AddRange(new string[] { "-timeout", "30" });
-			//args = testArgs.ToArray();
+    static void Main(string[] args)
+    {
+      //テスト引数
+      //var testArgs = new List<string>();
+      //testArgs.AddRange(new string[] { "-cputhd", "40" });
+      //testArgs.AddRange(new string[] { "-hddthd", "6" });
+      //testArgs.AddRange(new string[] { "-netthd", "10" });
+      //testArgs.AddRange(new string[] { "-duration", "20" });
+      //testArgs.AddRange(new string[] { "-timeout", "30" });
+      //args = testArgs.ToArray();
 
 
-			//Initialize
-			// try ~ catch で捕捉されてない例外を処理する
-			AppDomain.CurrentDomain.UnhandledException += ExceptionInfo.CurrentDomain_UnhandledException;
+      //Initialize
+      AppDomain.CurrentDomain.UnhandledException += ExceptionInfo.OnUnhandledException;  //例外を捕捉する
 
-			SystemEvents.PowerModeChanged += OnPowerModeChanged;											//suspend検知
+      SystemEvents.PowerModeChanged += OnPowerModeChanged;                     //suspend検知
 
-			Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.Idle;		//PerformanceCounter呼び出しの負荷が大きいのでIdle
+      Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.Idle;   //PerformanceCounter呼び出しの負荷が大きいのでIdle
 
-			//コンソールウィンドウを持っているか？
-			HaveConsole = true;
-			try { Console.Clear(); }
-			catch { HaveConsole = false; }							//ファイル等にリダイレクトされていると例外
+      //コンソールウィンドウを持っているか？
+      HaveConsole = true;
+      try { Console.Clear(); }
+      catch { HaveConsole = false; }   //ファイル等にリダイレクトされていると例外
 
-			//Args		
-			float[] systhd = ParseArgs(args);
-			duration = systhd[3];
-			timeout = systhd[4];
-			if (duration <= 0)
-			{
-				Exit_withIdle();													//終了
-			}
+      //Args    
+      float[] systhd = ParseCommandLine(args);
+      duration = systhd[3];
+      timeout = systhd[4];
+      if (duration <= 0)
+      {
+        Exit_withIdle();               //終了
+      }
 
-			//SystemMonitor											cpu				hdd				network		  	duration
-			systemMonitor = new SystemMonitor(systhd[0], systhd[1], systhd[2], (int)duration);
-
-
-
-			//
-			//main loop
-			//
-			Thread.Sleep(1500);													//systemMonitorと更新タイミングをずらす
-			startTime = DateTime.Now;
-			while (true)
-			{
-				lock (sync)
-				{
-					//画面表示更新
-					if (HaveConsole)
-					{
-						string text = GetTextMonitorState();
-						Console.Clear();
-						Console.Error.WriteLine(text);
-					}
-
-
-					//timeout?
-					if (0 < timeout
-								&& SystemIsSleep == false
-								&& timeout < (DateTime.Now - startTime).TotalSeconds
-							)
-					{
-						Exit_timeout();												//終了
-					}
-
-
-					//SystemIsIdle?
-					if (systemMonitor.SystemIsIdle()
-								&& SystemIsSleep == false
-								&& CheckProcess.NotExistBlack()
-							)
-					{
-						Exit_withIdle();											//終了
-					}
-				}
-
-				Thread.Sleep(3 * 1000);
-
-			}//while
-		}//func
+      //SystemMonitor                      cpu       hdd      network         duration
+      systemMonitor = new SystemMonitor(systhd[0], systhd[1], systhd[2], (int)duration);
 
 
 
+      //
+      //main loop
+      //
+      Thread.Sleep(1500);              //systemMonitorと更新タイミングをずらす
+      startTime = DateTime.Now;
+      while (true)
+      {
+        lock (sync)
+        {
+          //画面表示更新
+          if (HaveConsole)
+          {
+            string text = GetText_MonitorState();
+            Console.Clear();
+            Console.Error.WriteLine(text);
+          }
 
-		//
-		//GetTextMonitorState
-		//
-		static string GetTextMonitorState()
-		{
-			var state = new StringBuilder();
-			var black = (CheckProcess.NotExistBlack()) ? "○" : "×";
-			var idle = (systemMonitor.SystemIsIdle()) ? "○" : "×";
 
-			state.AppendLine("duration = " + duration + "    timeout = " + timeout);
-			state.AppendLine(systemMonitor.GetMonitorState());
-			state.AppendLine("NotExistBlack = " + black);
-			state.AppendLine("SysteIsIdle   = " + idle);
-			return state.ToString();
-		}
+          //timeout?
+          if (0 < timeout
+                && SystemIsSleep == false
+                && timeout < (DateTime.Now - startTime).TotalSeconds
+              )
+          {
+            Exit_timeout();            //終了
+          }
 
 
-		//
-		//終了
-		//
-		//Exit_timeout
-		static void Exit_timeout()
-		{
-			if (HaveConsole) Console.Clear();
-			Console.WriteLine("false");
-			Console.WriteLine(GetTextMonitorState());
+          //SystemIsIdle?
+          if (systemMonitor.SystemIsIdle()
+                && SystemIsSleep == false
+                && CheckProcess.NotExistBlack()
+              )
+          {
+            Exit_withIdle();           //終了
+          }
+        }
 
-			Thread.Sleep(2000);
+        Thread.Sleep(3 * 1000);
 
-			SystemEvents.PowerModeChanged -= OnPowerModeChanged;
-			Environment.Exit(1);												//ExitCode: 1
-		}
-
-		//Exit_withIdle
-		static void Exit_withIdle()
-		{
-			if (HaveConsole) Console.Clear();
-			Console.Write("true");
-
-			Thread.Sleep(2000);
-
-			SystemEvents.PowerModeChanged -= OnPowerModeChanged;
-			Environment.Exit(0);												//ExitCode: 0
-		}
+      }//while
+    }//func
 
 
 
 
-		//
-		//ParseArgs
-		//
-		#region ParseArgs
-		static float[] ParseArgs(string[] args)
-		{
+    //
+    //GetTextMonitorState
+    //
+    static string GetText_MonitorState()
+    {
+      var state = new StringBuilder();
+      var black = (CheckProcess.NotExistBlack()) ? "○" : "×";
+      var idle = (systemMonitor.SystemIsIdle()) ? "○" : "×";
 
-			float cputhd = DefValue.CpuThd, hddthd = DefValue.HddThd, netthd = DefValue.NetworkThd,
-						duration = DefValue.Durarion, timeout = DefValue.Timeout;
-
-			if (args.Count() == 0)
-			{
-				return new float[] { cputhd, hddthd, netthd, duration, timeout };
-			}
-			else
-			{
-				string name, param = "";
-				bool canparse = false;
-				float result = -1;
+      state.AppendLine("duration = " + duration + "    timeout = " + timeout);
+      state.AppendLine(systemMonitor.GetMonitorState());
+      state.AppendLine("NotExistBlack = " + black);
+      state.AppendLine("SysteIsIdle   = " + idle);
+      return state.ToString();
+    }
 
 
-				for (int i = 0; i < args.Length; i++)
-				{
-					name = args[i].ToLower();
+    //
+    //終了
+    //
+    //Exit_timeout
+    static void Exit_timeout()
+    {
+      if (HaveConsole) Console.Clear();
+      Console.WriteLine("false");
+      Console.WriteLine(GetText_MonitorState());
 
-					if (name.IndexOf("-") == 0 || name.IndexOf("/") == 0)
-						name = name.Substring(1, name.Length - 1);									//  - / をはずす
-					else
-						continue;																										//  - / がない
+      Thread.Sleep(2000);
 
+      SystemEvents.PowerModeChanged -= OnPowerModeChanged;
+      Environment.Exit(1);             //ExitCode: 1
+    }
 
-					if (i < args.Length - 1)
-					{
-						param = args[i + 1];
-						canparse = float.TryParse(param, out result);
+    //Exit_withIdle
+    static void Exit_withIdle()
+    {
+      if (HaveConsole) Console.Clear();
+      Console.Write("true");
 
-						switch (name)
-						{
-							case "cputhd":
-								cputhd = DefValue.CpuThd;
-								if (canparse) cputhd = result;
-								break;
+      Thread.Sleep(2000);
 
-							case "hddthd":
-								hddthd = DefValue.HddThd;
-								if (canparse) hddthd = result;
-								break;
-
-							case "netthd":
-								netthd = DefValue.NetworkThd;
-								if (canparse) netthd = result;
-								break;
-
-							case "dur":
-							case "duration":
-								duration = DefValue.Durarion;
-								if (canparse) duration = result;
-								break;
-
-							case "timeout":
-								timeout = DefValue.Timeout;
-								if (canparse) timeout = result;
-								break;
-
-						}//switch
-					}//if
-
-				}//for
-			}//if
-
-			return new float[] { cputhd, hddthd, netthd, duration, timeout };
-
-		}//function
-		#endregion
+      SystemEvents.PowerModeChanged -= OnPowerModeChanged;
+      Environment.Exit(0);             //ExitCode: 0
+    }
 
 
-		//
-		//PowerModeChangedEvent
-		//
-		#region PowerModeChangedEvent
-		//PowerModes.Suspend --> PowerModes.Resumeの順で発生するとはかぎらない。
-		//・PowerModes.Resume --> [windows sleep] --> PowerModes.Suspend
-		//・                      [windows sleep] --> PowerModes.Resume  --> PowerModes.Suspend
-		//の順でくることもある。
-		static void OnPowerModeChanged(object sender, PowerModeChangedEventArgs e)
-		{
-			lock (sync)
-			{
-				//Suspend
-				if (e.Mode == PowerModes.Suspend
-					&& 10 < (DateTime.Now - lastResumeTime).TotalSeconds)				//前回のリジュームから１０秒たっている？
-				{
-					SystemIsSleep = true;
-					systemMonitor.TimerStop();
-				}
-				//Resume
-				else if (e.Mode == PowerModes.Resume)
-				{
-					SystemIsSleep = true;
-					systemMonitor.TimerStop();
 
-					Thread.Sleep(10 * 1000);																		//リジューム直後は処理しない
 
-					startTime = DateTime.Now;
-					lastResumeTime = DateTime.Now;
-					systemMonitor.TimerStart();
-					SystemIsSleep = false;
-				}
-			}
-		}
-		#endregion
+    //
+    //ParseCommandLine
+    //
+    #region ParseCommandLine
+    static float[] ParseCommandLine(string[] args)
+    {
 
-	}
+      float cputhd = DefValue.CpuThd, hddthd = DefValue.HddThd, netthd = DefValue.NetworkThd,
+            duration = DefValue.Durarion, timeout = DefValue.Timeout;
+
+      if (args.Count() == 0)
+      {
+        return new float[] { cputhd, hddthd, netthd, duration, timeout };
+      }
+      else
+      {
+        string name, param = "";
+        bool canparse = false;
+        float result = -1;
+
+
+        for (int i = 0; i < args.Length; i++)
+        {
+          name = args[i].ToLower();
+
+          if (name.IndexOf("-") == 0 || name.IndexOf("/") == 0)
+            name = name.Substring(1, name.Length - 1);     //  - / をはずす
+          else
+            continue;
+
+
+          if (i < args.Length - 1)
+          {
+            param = args[i + 1];
+            canparse = float.TryParse(param, out result);
+
+            switch (name)
+            {
+              case "cputhd":
+                cputhd = DefValue.CpuThd;
+                if (canparse) cputhd = result;
+                break;
+
+              case "hddthd":
+                hddthd = DefValue.HddThd;
+                if (canparse) hddthd = result;
+                break;
+
+              case "netthd":
+                netthd = DefValue.NetworkThd;
+                if (canparse) netthd = result;
+                break;
+
+              case "dur":
+              case "duration":
+                duration = DefValue.Durarion;
+                if (canparse) duration = result;
+                break;
+
+              case "timeout":
+                timeout = DefValue.Timeout;
+                if (canparse) timeout = result;
+                break;
+
+            }//switch
+          }//if
+
+        }//for
+      }//if
+
+      return new float[] { cputhd, hddthd, netthd, duration, timeout };
+
+    }//function
+    #endregion
+
+
+    //
+    //PowerModeChangedEvent
+    //
+    #region PowerModeChangedEvent
+    //  PowerModes.Suspend　→　[windows sleep]　→　PowerModes.Resumeの順で発生するとはかぎらない。
+    //　　　　　　　　　　　　　[windows sleep]　→　PowerModes.Suspend　→　PowerModes.Resume 又は
+    //　                        [windows sleep]　→　PowerModes.Resume　 →　PowerModes.Suspend
+    //の順でイベントがくることもある。
+    static void OnPowerModeChanged(object sender, PowerModeChangedEventArgs e)
+    {
+      lock (sync)
+      {
+        //Suspend
+        if (e.Mode == PowerModes.Suspend
+          && 10 < (DateTime.Now - lastResumeTime).TotalSeconds)      //前回のリジュームから１０秒たっている？
+        {
+          SystemIsSleep = true;
+          systemMonitor.TimerStop();
+        }
+        //Resume
+        else if (e.Mode == PowerModes.Resume)
+        {
+          SystemIsSleep = true;
+          systemMonitor.TimerStop();
+
+          Thread.Sleep(10 * 1000);                         //リジューム直後は処理しない
+
+          startTime = DateTime.Now;
+          lastResumeTime = DateTime.Now;
+          systemMonitor.TimerStart();
+          SystemIsSleep = false;
+        }
+      }
+    }
+    #endregion
+
+  }
 
 
 
